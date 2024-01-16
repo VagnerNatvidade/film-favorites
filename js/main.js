7cc903fe0ac65efd795206783b0d22fcfaecb115
vagnerNatividade <vagnerflorindojunior@gmail.com>
2024-01-15 23:19:19 -0300

    prepara o front do projeto

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/*
+        <li class="movie">
+          <img
+          src="https://br.web.img3.acsta.net/c_310_420/pictures/21/11/08/16/02/3963914.png"
+          alt="Filme Homem Aranha"
+          />
+        </li>
+*/
